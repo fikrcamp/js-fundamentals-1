ee3f8b73f9786e2014a6fbca4d686321dd18886e
hafuus <hafuus17@gmail.com>
2022-04-05 10:46:29 +0300

    done

--- exercise.js
+++ exercise.js
@@ -1,2 +1,68 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let countary = "somaliland"
+let continant = "africa"
+let population = 40000000
+console.log(`my countary is ${countary} in the ${continant} and the population is ${population}`)
+ 
+let language = "somali"
+let oncoast = true
+if(oncoast===true){
+    console.log(`somaliland is ${oncoast} and speaks ${language}`)
+}
+
+
+let half = population /2
+let increase = population ++
+console.log(`the half is ${half} and if increase by 1 is ${increase}`)
+
+ let finland = 60000000
+ console.log(finland > population)
+ average = 3300000
+ if (average > population){
+     console.log(average > population)
+ }
+ 
+description = countary
+console.log(`${countary} is in ${continant} that have ${population} nd speaks ${language}`)
+countriespopulaton = "33million"
+if(countriespopulaton> population){
+    console.log("above average")
+} else{
+    console.log("lower average")
+}
+
+ahmedspopu = 500000
+oncoast = false
+if(ahmedspopu && oncoast == population && oncoast){
+  console.log ("u fit")
+} else{
+    console.log("you dont fit")
+}
+
+language = "english"
+if (language == "chinese"){
+    console.log ("most native lan")
+}
+else if (language == "spanish"){
+    console.log (" 2nd most native lan")
+} else if (language == "english"){
+    console.log (" third place")
+}
+else if (language == "hindi"){
+    console.log (" 4")
+}
+else if (language == "arabic"){
+    console.log (" 5th most native lan")
+}
+else {
+    console.log ("great language")
+}
+
+
+population>average ? console.log('above average') : console.log("below average")
+
+
+
+
