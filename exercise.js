922cac2361e6f351d524ac6ce01b5ee20e85df85
mustafe abdirahman <111310904+carabzl@users.noreply.github.com>
2022-11-09 23:42:11 +0300

    js fundamentals I assignment

--- exercise.js
+++ exercise.js
@@ -1,2 +1,65 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+// Variables and Values exercise
+let country = "norway"
+let continent = "europe"
+let population = 6000000
+console.log(country,continent,population)
+let language = "norwegian"
+let oncoast = true
+//Basic operators exercise
+let _halfpop = population / 2
+let pluspop = population + 1
+console.log(pluspop)
+let finlandpop = 6000000
+if(population >= finlandpop){
+    console.log("yes more then or same population")
+}else{
+    console.log("no less then")
+}
+let description =(`${country} is in ${continent} and it's ${population} million people speak ${language}`)
+console.log(description)
+//If & Else statements 
+if(population > 33000000){
+    console.log(`${country}  population is above average`)
+}else{
+    console.log(`${country} population is below the average`)
+}
+//Logical Operators
+if(population < 50000000 && oncoast === false){
+    console.log("Ahmed you can live in norway ")
+}else{
+    console.log("Ahmed you can't live in norway")
+}
+//If & else Statements 2
+let langu = "somali";
+if(langu === "chinese" || langu === "mandarin"){
+    console.log("MOST number of native speakers");
+}
+else if(langu === "spanish"){
+    console.log("2nd place in number of native speakers");
+}
+else if(langu === "hindi"){
+    console.log("Number 4");
+}
+else if(langu === "arabic"){
+    console.log("5th most spoken language");
+}
+else {
+   console.log("Great languages too ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
