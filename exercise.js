8f91f4a294c552199b0a5fa3554a61ceea0e425e
sakariye1scl <sakariye1scl>
2022-12-25 13:44:06 +0300

    finished the assignment

--- exercise.js
+++ exercise.js
@@ -1,2 +1,52 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let  country = "Somaliland";
+let continent = "Africa";
+let population = 5;
+let x = 2;
+let y = 1;
+let language = "Somali";
+let oncoast = false ;
+let thereIsNot = true;
+
+(5 / 2 + 1)
+
+
+let finlandPopulation = 6;
+let somalilandPopulation = 5;
+if (finlandPopulation > somalilandPopulation){
+  console.log("somaliland is less the  population of finland")
+}
+
+
+description = `${country} is in, ${continent} and its, ${population} million and they  speak, ${language}`
+console.log(description)
+
+if (population > 33){
+  conosle.log(`${country} population is above average`)
+}else {
+  console.log(`${country} population is below average`)
+}
+
+if (population < 50  ){
+  console.log(`you should live in ${country}`)
+  
+}
+
+let firstLanguage = "chinese or mandarin"
+let secondLanguage = "spanish"
+let thirdLanguage = "english"
+let forthLanguage = "hindi"
+let fifthLanguage = "arabic"
+
+if (firstLanguage == "chinese or mandarin" ){
+    console.log(`Most number of native speakers`)
+}else if (secondLanguage == "spanish"){
+    console.log("2nd place in number of native speakers")
+} else if (thirdLanguage == english ){
+    console.log("3rd  place")
+}else if (forthLanguage == "arabic"){
+    console.log("5th most spoken language")
+}else {
+    console.log("all other languages are greate language")
+}
