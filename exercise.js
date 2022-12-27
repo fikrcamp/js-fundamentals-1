f2ac137baa33b440321ccfad34127bc7cbb946f6
nemo <nemoabdi409@gmail.com>
2022-12-27 11:32:36 -0800

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,2 +1,56 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+//Variables and Values exercise//
+
+let continent = "Africa"
+let country = "Somaliland"
+let population = 5,700,000
+
+console.log(continent,country,population)
+
+let language = "Somali"
+let oncoast = "false"
+
+console.log(language,oncoast)
+
+//Basic operators exercise//
+for (let population = 5,700,000 ; population/){
+    console.log(population+)
+}
+let description = "Somaliland is in Africa and it has 5.7million population, people speak Somali"
+
+//If & Else statements//
+
+if (population < 33,000,000){
+    console.log("Somaliland's population is bellow average")
+}
+
+//Logical Operators//
+
+if ( population < 50,000,000 && oncoast = "false"){
+    console.log("you should live in Somaliland")
+}
+else if (population > 50,000,000 && oncoast = "true"){
+    console.log("Somaliland doesn't meet your criteria")
+}
+//If & Else statements//
+
+if (Language =  "chinese or mandarin"){
+    console.log("MOST number of native speakers")
+}
+else if (Language = "spanish"){
+    console.log("2nd place in number of native speakers")
+}
+else if ( Language = "English"){
+    console.log("3rd place")
+}
+else if ( Language = "Hindi"){
+    console.log("Number 4")
+}
+else if (Language = "Arabic"){
+    console.log("5th most spoken language")
+}
+else {
+    console.log("Great languages too :)")
+}
